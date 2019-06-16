9245eb5808f52d7aa1a71ac2a59091513088af81
yumlonne <yumlonne@gmail.com>
2019-06-16 12:25:34 +0900

    impl main

--- src/Main.elm
+++ src/Main.elm
@@ -4,7 +4,9 @@ import Brainfuck
 import Brainfuck.Operation as Operation
 import Browser exposing (Document)
 import Element exposing (..)
+import Element.Background as Background
 import Element.Border as Border
+import Element.Font as Font
 import Element.Input as Input
 import Html exposing (Html)
 
@@ -29,6 +31,8 @@ main =
 type alias Model =
     { brainfuck : Brainfuck.Model
     , program : String
+    , inputLog : String
+    , errorMsg : String
     }
 
 
@@ -40,6 +44,8 @@ init : () -> ( Model, Cmd Msg )
 init _ =
     ( { brainfuck = Brainfuck.init defaultArrayLength
       , program = ""
+      , inputLog = ""
+      , errorMsg = ""
       }
     , Cmd.none
     )
@@ -88,12 +94,12 @@ update msg model =
                         newBrainfuck =
                             Brainfuck.execAll ops model.brainfuck
                     in
-                    ( { model | brainfuck = newBrainfuck }
+                    ( { model | brainfuck = newBrainfuck, errorMsg = "" }
                     , Cmd.none
                     )
 
-                Err errMsg ->
-                    ( model
+                Err errorMsg ->
+                    ( { model | errorMsg = errorMsg }
                     , Cmd.none
                     )
 
@@ -105,7 +111,7 @@ update msg model =
                 newBrainfuck =
                     List.foldl Brainfuck.input model.brainfuck chars
             in
-            ( { model | brainfuck = newBrainfuck }
+            ( { model | brainfuck = newBrainfuck, inputLog = model.inputLog ++ input }
             , Cmd.none
             )
 
@@ -115,41 +121,54 @@ view model =
     { title = "BrainF*ck"
     , body =
         [ layout [] <|
-            column []
-                [ el [] <|
-                    Input.multiline []
-                        { label = Input.labelAbove [] <| text "your program"
-                        , onChange = OnInputProgram
-                        , placeholder = Nothing
-                        , spellcheck = False
-                        , text = model.program
-                        }
-                , el [] <|
-                    Input.button []
-                        { label = text "実行"
-                        , onPress = Just OnClickExecuteButton
-                        }
-                , el [] <|
-                    let
-                        labelText =
-                            "プログラムへ入力する"
+            row [ width fill, height fill, padding 30 ]
+                [ column [ centerX, width <| px 600, spacing 40 ]
+                    [ el [ centerX ] <|
+                        Input.multiline [ centerX, width <| px 400, height <| px 480, Font.size 15 ]
+                            { label = Input.labelAbove [ centerX ] <| text "↓ここにBrainf*ckプログラムを入力する"
+                            , onChange = OnInputProgram
+                            , placeholder = Nothing
+                            , spellcheck = False
+                            , text = model.program
+                            }
+                    , row [ alignRight, paddingXY 100 0 ]
+                        [ el [ paddingXY 50 0, Font.color <| rgb255 228 64 64 ] <|
+                            text model.errorMsg
+                        , el [] <|
+                            Input.button [ Background.color <| rgb255 128 128 228, width <| px 80, height <| px 50, centerX, centerY ]
+                                { label = el [ centerX ] <| text "実行"
+                                , onPress = Just OnClickExecuteButton
+                                }
+                        ]
+                    , row [ centerX, height <| px 40 ]
+                        [ el [ centerX, height <| px 40 ] <|
+                            let
+                                labelText =
+                                    "入力"
 
-                        label =
-                            if model.brainfuck.waitingInput then
-                                labelText ++ " !!"
+                                label =
+                                    if model.brainfuck.waitingInput then
+                                        labelText ++ " (待機中...)"
 
-                            else
-                                labelText
-                    in
-                    Input.text []
-                        { onChange = OnInput
-                        , text = ""
-                        , placeholder = Nothing
-                        , label = Input.labelLeft [] <| text label
-                        }
-                , el [] <|
-                    Input.multiline []
-                        { label = Input.labelAbove [] <| text "result"
+                                    else
+                                        labelText
+                            in
+                            Input.text [ height <| px 40 ]
+                                { onChange = OnInput
+                                , text = ""
+                                , placeholder = Nothing
+                                , label = Input.labelLeft [ centerY ] <| text label
+                                }
+                        , el [ centerY, padding 10 ] <|
+                            Input.button [ width <| px 25, height <| px 40, Background.color <| rgb255 192 192 192 ]
+                                { label = el [ centerX, Font.color <| rgb255 0 0 0 ] <| text "\\0"
+                                , onPress = Just << OnInput << String.fromChar <| Char.fromCode 0
+                                }
+                        ]
+                    ]
+                , el [ centerX ] <|
+                    Input.multiline [ centerX, width <| px 500, height <| px 650, padding 30, Font.size 15 ]
+                        { label = Input.labelAbove [ centerX ] <| text "出力"
                         , onChange = always NoOp
                         , placeholder = Nothing
                         , spellcheck = False
