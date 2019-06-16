69514b1288722c69671d3202dc1e2c54d8513271
yumlonne <yumlonne@gmail.com>
2019-06-16 10:30:38 +0900

    WIP impl Main

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,17 @@
 module Main exposing (main)
 
-import Browser
+import Brainfuck
+import Brainfuck.Operation as Operation
+import Browser exposing (Document)
+import Element exposing (..)
+import Element.Border as Border
+import Element.Input as Input
+import Html exposing (Html)
+
+
 
 -- MAIN
+
 
 main =
     Browser.document
@@ -14,9 +23,122 @@ main =
 
 
 
+-- MODEL
 
-init : () -> ( (), Cmd () )
-init = Debug.todo "impl"
-subscriptions = Debug.todo "impl"
-update = Debug.todo "impl"
-view = Debug.todo "impl"
+
+type alias Model =
+    { brainfuck : Brainfuck.Model
+    , program : String
+    , input : String
+    }
+
+
+defaultArrayLength =
+    1000
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { brainfuck = Brainfuck.init defaultArrayLength
+      , program = ""
+      , input = ""
+      }
+    , Cmd.none
+    )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions model =
+    Sub.none
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+    | OnInputProgram String
+    | OnClickExecuteButton
+    | OnInput String
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model
+            , Cmd.none
+            )
+
+        OnInputProgram program ->
+            ( { model | program = program }
+            , Cmd.none
+            )
+
+        OnClickExecuteButton ->
+            let
+                parsed =
+                    Operation.parse model.program
+            in
+            case parsed of
+                Ok ops ->
+                    let
+                        newBrainfuck =
+                            Brainfuck.execAll ops model.brainfuck
+                    in
+                    ( { model | brainfuck = newBrainfuck }
+                    , Cmd.none
+                    )
+
+                Err errMsg ->
+                    ( model
+                    , Cmd.none
+                    )
+
+        OnInput input ->
+            ( { model | input = input }
+            , Cmd.none
+            )
+
+
+view : Model -> Document Msg
+view model =
+    { title = "BrainF*ck"
+    , body =
+        [ layout [] <|
+            column []
+                [ el [] <|
+                    Input.multiline []
+                        { label = Input.labelAbove [] <| text "your program"
+                        , onChange = OnInputProgram
+                        , placeholder = Nothing
+                        , spellcheck = False
+                        , text = model.program
+                        }
+                , el [] <|
+                    Input.button []
+                        { label = text "実行"
+                        , onPress = Just OnClickExecuteButton
+                        }
+                , el [] <|
+                    Input.text []
+                        { onChange = OnInput
+                        , text = model.input
+                        , placeholder = Nothing
+                        , label = Input.labelLeft [] <| text "プログラムへ入力する"
+                        }
+                , el [] <|
+                    Input.multiline []
+                        { label = Input.labelAbove [] <| text "result"
+                        , onChange = always NoOp
+                        , placeholder = Nothing
+                        , spellcheck = False
+                        , text = model.brainfuck.outputString
+                        }
+                ]
+        ]
+    }
